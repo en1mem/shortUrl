0806ab21da30724d846ce3d61a419620d78e6b4b
en1mem <germanorumgepp@mail.ru>
2021-03-28 00:59:02 +0500

    gepp/testtask - исправил багу с отсутствием создания редирект обжектов при редиректе

--- src/main/java/ru/german/service/RedirectService.java
+++ src/main/java/ru/german/service/RedirectService.java
@@ -1,11 +1,13 @@
 package ru.german.service;
 
+import generated.tables.pojos.Redirect;
 import org.jooq.tools.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import ru.german.repository.RedirectRepository;
 import ru.german.repository.UrlRepository;
 
 @Service
@@ -13,6 +15,9 @@ public class RedirectService {
     
     @Autowired
     UrlRepository repository;
+
+    @Autowired
+    RedirectRepository redirectRepository;
 
     @Value("${currentHost}")
     String currentHost;
@@ -22,6 +27,10 @@ public class RedirectService {
     public String doRedirect(String shortUrl) {
         Long urlId = repository.getIdByShortUrl(currentHost + "/redirect/" + shortUrl);
         if (urlId != null) {
+            Redirect redirect = new Redirect();
+            redirect.setUrlId(urlId);
+            redirectRepository.insert(redirect);
+
             repository.redirectCounter(urlId);
             return repository.getFullUrlById(urlId);
         } else {
